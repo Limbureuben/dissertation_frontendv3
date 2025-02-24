fb8be3c05f7db0fa1020b453925997054a68660a
Limbureuben <limbureuben4@gmail.com>
2025-02-24 15:05:34 +0300

    chnages

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AdminHeaderComponent } from './admin-header/admin-header.component';
+import { AdminSidebarComponent } from './admin-sidebar/admin-sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AdminDashboardComponent
+    AdminDashboardComponent,
+    AdminHeaderComponent,
+    AdminSidebarComponent
   ],
   imports: [
     CommonModule,
