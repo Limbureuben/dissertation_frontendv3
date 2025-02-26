2100960465763d63f275ea24fc29ccf0d79cfb0a
Limbureuben <limbureuben4@gmail.com>
2025-02-26 17:57:35 +0300

    chnages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { FooterComponent } from './sharing/footer/footer.component';
 import { OnlineComponent } from './sharing/online/online.component';
 import { AdminHeaderComponent } from './admin/admin-header/admin-header.component';
 import { AdminSidebarComponent } from './admin/admin-sidebar/admin-sidebar.component';
+import { UserHeaderComponent } from './user/user-header/user-header.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/homepage', pathMatch: 'full'},
@@ -37,6 +38,18 @@ const routes: Routes = [
     component: AdminSidebarComponent,
     loadChildren: () =>
       import('./admin/admin.module').then((m) =>m.AdminModule)
+  },
+  {
+    path: 'app',
+    component: UserHeaderComponent,
+    loadChildren: () =>
+      import('./user/user.module').then((m) =>m.UserModule)
+  },
+  {
+    path: 'app',
+    component: UserHeaderComponent,
+    loadChildren: () =>
+      import('./user/user.module').then((m) =>m.UserModule)
   }
 ];
 
