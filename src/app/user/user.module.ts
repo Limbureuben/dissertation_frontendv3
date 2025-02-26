f9a3f4f32321d86630d0356a807f2d7e23545c9d
Limbureuben <limbureuben4@gmail.com>
2025-02-26 17:54:30 +0300

    changes

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { UserHeaderComponent } from './user-header/user-header.component';
+import { UserFooterComponent } from './user-footer/user-footer.component';
 
 
 @NgModule({
   declarations: [
-    UserDashboardComponent
+    UserDashboardComponent,
+    UserHeaderComponent,
+    UserFooterComponent
   ],
   imports: [
     CommonModule,
