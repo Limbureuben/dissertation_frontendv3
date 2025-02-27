6786a54e58e6fda64d48edcc8f10b8c5eaf51f91
Limbureuben <limbureuben4@gmail.com>
2025-02-27 17:30:15 +0300

    chanes

--- src/app/sharing/login/login.component.ts
+++ src/app/sharing/login/login.component.ts
@@ -64,7 +64,7 @@ export class LoginComponent implements OnInit{
             return;
           }
           if(user.emailVerified) {
-            this.router.navigate(['/']);
+            this.router.navigate(['/map-display']);
           } else {
             this.toastr.warning('Please verify your email first.');
           }
