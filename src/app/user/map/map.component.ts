334a4e7ff1ebb5d500bd4ab44331a7376e1f7679
Limbureuben <limbureuben4@gmail.com>
2025-03-06 17:49:52 +0300

    changes

--- src/app/user/map/map.component.ts
+++ src/app/user/map/map.component.ts
@@ -16,9 +16,20 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('map') private mapContainer!: ElementRef<HTMLElement>;
 
   openSpaces = [
-    { name: "Park A", lng: -6.658270, lat: 39.185784 },
-    { name: "Park B", lng: -6.794852, lat: 39.116709 },
-    { name: "Garden C", lng: -6.800974, lat: 39.079890 }
+
+    { name: "Consulting Engineering", lng: 39.185784, lat: -6.658270 },
+    { name: "Msigan", lng: 39.116709, lat: -6.794852 },
+    { name: "KKT Church Boko", lng: 39.144646, lat: -6.624518 },
+    { name: "Bunju", lng: 39.144157, lat: -6.622651 },
+    { name: "Eco Fasten", lng: 39.230099, lat: -6.774133 },
+    { name: "Bora football field", lng: 39.235777, lat: -6.778251 },
+    { name: "Masjid", lng: 39.233556, lat: -6.781032 },
+    { name: "Mwenge", lng: 39.226800, lat: -6.768383 },
+    { name: "Avan garden", lng: 39.222820, lat: -6.770813 },
+    { name: "Ardhi football", lng: 39.216423, lat: -6.764396 },
+    { name: "Bunju", lng: 39.144157, lat: -6.622651 },
+    { name: "Bunju", lng: 39.144157, lat: -6.622651 },
+
   ];
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
@@ -27,12 +38,14 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
     config.apiKey = '9rtSKNwbDOYAoeEEeW9B';
   }
 
+
   ngAfterViewInit() {
     if (isPlatformBrowser(this.platformId)) {
       this.map = new Map({
         container: this.mapContainer.nativeElement,
-        style: MapStyle.STREETS,
-        center: [-6.800974, 39.079890], // Center on Tokyo
+        // style: MapStyle.SATELLITE,
+        style: 'https://api.maptiler.com/maps/hybrid/style.json?key=9rtSKNwbDOYAoeEEeW9B',
+        center: [39.185784, -6.658270], // Center on Tokyo
         zoom: 14
       });
 
@@ -52,5 +65,20 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
 
   ngOnDestroy() {
     this.map?.remove();
+  }
+
+  searchLocation(event: Event) {
+    const query = (event.target as HTMLInputElement).value;
+    if (!query) return;
+
+    fetch(`https://api.maptiler.com/geocoding/${query}.json?key=9rtSKNwbDOYAoeEEeW9B`)
+    .then(res => res.json())
+    .then(data => {
+      if (data.features.length > 0) {
+        const { center } = data.features[0]; // Get the first result
+        this.map?.flyTo({ center, zoom: 14 });
+      }
+    })
+    .catch(err => console.error("Error fetching location:", err));
   }
 }
