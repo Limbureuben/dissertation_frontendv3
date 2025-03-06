bf3a19734cd1f47828f44d7de9bd4b10dc8e84d6
Limbureuben <limbureuben4@gmail.com>
2025-03-06 15:45:39 +0300

    chnages

--- src/app/user/map/map.component.ts
+++ src/app/user/map/map.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
-import { Map, MapStyle, config } from '@maptiler/sdk';
+import { Map, MapStyle, config, Marker } from '@maptiler/sdk';
 
 import '@maptiler/sdk/dist/maptiler-sdk.css';
 
@@ -8,13 +8,12 @@ import '@maptiler/sdk/dist/maptiler-sdk.css';
   selector: 'app-map',
   standalone: false,
   templateUrl: './map.component.html',
-  styleUrl: './map.component.scss'
+  styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
   map: Map | undefined;
 
-  @ViewChild('map')
-  private mapContainer!: ElementRef<HTMLElement>;
+  @ViewChild('map') private mapContainer!: ElementRef<HTMLElement>;
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
@@ -32,10 +31,26 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
         center: [initialState.lng, initialState.lat],
         zoom: initialState.zoom
       });
+
+      // Add marker
+      const marker = new Marker()
+        .setLngLat([139.753, 35.6844]) // Set coordinates for the marker
+        .addTo(this.map); // Add marker to map
+
+      // Add click event to the marker
+      marker.getElement().addEventListener('click', () => {
+        this.showReportForm(); // Show the form when marker is clicked
+      });
     }
   }
 
   ngOnDestroy() {
     this.map?.remove();
+  }
+
+  showReportForm() {
+    // Code to display a form for the user to report the issue
+    alert("Form will pop up here for reporting.");
+    // You can trigger a modal or a form dialog here
   }
 }
