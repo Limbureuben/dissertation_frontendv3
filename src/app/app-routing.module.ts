64cef61ba899632420e5525b7b18b3e7dde7e42a
Limbureuben <limbureuben4@gmail.com>
2025-03-11 12:56:27 +0300

    chnages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import { ReportFormComponent } from './user/report-form/report-form.component';
 import { AdminFooterComponent } from './admin/admin-footer/admin-footer.component';
 import { DoughnutChartComponent } from './admin/doughnut-chart/doughnut-chart.component';
 import { CommonHeaderComponent } from './sharing/common-header/common-header.component';
+import { MapAdminComponent } from './admin/map-admin/map-admin.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/user-home', pathMatch: 'full'},
@@ -85,6 +86,12 @@ const routes: Routes = [
     component: CommonHeaderComponent,
     loadChildren: () =>
       import('./sharing/sharing.module').then((m) =>m.SharingModule)
+  },
+  {
+    path: 'app',
+    component: MapAdminComponent,
+    loadChildren: () =>
+      import('./admin/admin.module').then((m) =>m.AdminModule)
   }
 ];
 
