61cd95ce0625b11544a45008d3bab1f3520761f3
Limbureuben <limbureuben4@gmail.com>
2025-03-22 00:50:30 +0300

    changes

--- src/app/user/anonymousreport/anonymousreport.component.ts
+++ src/app/user/anonymousreport/anonymousreport.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OpenspaceService } from '../../service/openspace.service';
 
 @Component({
   selector: 'app-anonymousreport',
@@ -6,6 +7,38 @@ import { Component } from '@angular/core';
   templateUrl: './anonymousreport.component.html',
   styleUrl: './anonymousreport.component.scss'
 })
-export class AnonymousreportComponent {
+export class AnonymousreportComponent implements OnInit{
+  reports: any[] = [];
+  sessionId: string = '';
+  loading: boolean = false;
+  errorMessage: string = '';
+
+  constructor(private openSpaceService: OpenspaceService) {}
+
+  ngOnInit(): void {
+    const storedSessionId = localStorage.getItem('session_id');
+
+    if (!storedSessionId) {
+      this.errorMessage = 'No session ID found. Please submit a report first.';
+      return;
+    }
+    this.sessionId = storedSessionId;
+  this.fetchReports();
+  }
+
+  fetchReports() {
+    this.loading = true;
+    this.openSpaceService.getAnonymousReports(this.sessionId).subscribe({
+      next: (response: any) => {
+        this.reports = response.data.anonymousReports;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('Error fetching reports:', error);
+        this.errorMessage = 'Failed to fetch reports. Please try again later.';
+        this.loading = false;
+      }
+    })
+  }
 
 }
