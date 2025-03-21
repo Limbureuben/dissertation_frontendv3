8102dec885a784c1851753ad22dca48b26919c4f
Limbureuben <limbureuben4@gmail.com>
2025-03-21 16:27:17 +0300

    changes

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -42,9 +42,21 @@ export class AuthService {
   }
 
   Logout() {
+    const accessToken = localStorage.getItem('success_token');
+    const refreshToken = localStorage.getItem('refresh_token');
+
+    console.log('Before Logout:');
+    console.log('Access Token:', accessToken);
+    console.log('Refresh Token:', refreshToken);
+
     localStorage.removeItem('success_token');
     localStorage.removeItem('refresh_token');
     localStorage.removeItem('is_staff');
+
+    console.log('After Logout:');
+    console.log('Access Token:', localStorage.getItem('success_token')); // Should be null
+    console.log('Refresh Token:', localStorage.getItem('refresh_token')); // Should be null
+
 
     this.toast.success('Logout successful!', 'Success', {
       positionClass: 'toast-top-right'
