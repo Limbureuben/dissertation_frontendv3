2a28e90ac90f5c7a77f2b790870faac62b2a325c
Limbureuben <limbureuben4@gmail.com>
2025-03-24 15:22:17 +0300

    changes

--- src/app/user/view-history/view-history.component.ts
+++ src/app/user/view-history/view-history.component.ts
@@ -1,11 +1,73 @@
-import { Component } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { OpenspaceService } from '../../service/openspace.service';
+import { ToastrService } from 'ngx-toastr';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-view-history',
   standalone: false,
   templateUrl: './view-history.component.html',
-  styleUrl: './view-history.component.scss'
+  styleUrl: './view-history.component.scss',
+  animations: [
+    // Slide table from right when opening
+    trigger('tableEnterAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateX(50px)' }),
+        animate('500ms ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+      ])
+    ]),
+
+    // Animate row additions & deletions
+    trigger('rowAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-10px)' }),
+        animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ opacity: 0, transform: 'translateY(10px)' }))
+      ])
+    ])
+  ]
 })
 export class ViewHistoryComponent {
+
+  dataSource = new MatTableDataSource<any>([]);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    private openSpaceService: OpenspaceService,
+    private toastr: ToastrService,
+    private dialog: MatDialog
+  ) {}
+
+
+  ngOnInit(): void {
+      this.loadReport()
+  }
+
+  loadReport() {
+    this.openSpaceService.getAllHistory().subscribe((data) => {
+      this.dataSource.data = data;
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+
 
 }
