16622c88fae2a4891ddab5e923f7cf6625512a24
Limbureuben <limbureuben4@gmail.com>
2025-03-26 11:12:40 +0300

    changes

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { error } from 'console';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,6 +8,25 @@ import { Component } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+ profile: any
+ loading = true
+ errorMessage: string = '';
+
+ constructor(private authservice: AuthService) {}
+
+ ngOnInit(): void {
+     this.authservice.getUserProfile().subscribe({
+      next: (data) => {
+        this.profile = data;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('Error fetching profile:', error);
+        this.errorMessage = 'Failed to load profile. Please try again.';
+        this.loading = false;
+      }
+     });
+ }
 
 }
