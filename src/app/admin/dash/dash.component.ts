ff9f27f07191a3bbf634f0967b6e74cb6a647f1f
Limbureuben <limbureuben4@gmail.com>
2025-04-04 19:43:06 +0300

    changes

--- src/app/admin/dash/dash.component.ts
+++ src/app/admin/dash/dash.component.ts
@@ -1,10 +1,23 @@
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dash',
   standalone: false,
   templateUrl: './dash.component.html',
-  styleUrl: './dash.component.scss'
+  styleUrl: './dash.component.scss',
+  animations: [
+      trigger('listAnimation', [
+        transition(':enter', [
+          query('.stat-card', [
+            style({ opacity: 0, transform: 'scale(0.8)' }),
+            stagger(200, [
+              animate('500ms ease-out', style({ opacity: 1, transform: 'scale(1)' }))
+            ])
+          ], { optional: true })
+        ])
+      ])
+    ]
 })
 export class DashComponent {
 
