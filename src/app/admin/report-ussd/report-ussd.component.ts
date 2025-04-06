9d9ebb8b5911dbd9507c00479026e0f3f63ec9f2
Limbureuben <limbureuben4@gmail.com>
2025-04-06 23:04:59 +0300

    changes

--- src/app/admin/report-ussd/report-ussd.component.ts
+++ src/app/admin/report-ussd/report-ussd.component.ts
@@ -1,10 +1,30 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-report-ussd',
   standalone: false,
   templateUrl: './report-ussd.component.html',
-  styleUrl: './report-ussd.component.scss'
+  styleUrl: './report-ussd.component.scss',
+  animations: [
+    trigger('tableEnterAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateX(50px)' }),
+        animate('500ms ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+      ])
+    ]),
+
+    // Animate row additions & deletions
+    trigger('rowAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-10px)' }),
+        animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ opacity: 0, transform: 'translateY(10px)' }))
+      ])
+    ])
+  ]
 })
 export class ReportUssdComponent {
 
