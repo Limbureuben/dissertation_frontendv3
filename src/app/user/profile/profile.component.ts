d4a3c9ca17c19030d9a0389d6ad3515da1d84be1
Limbureuben <limbureuben4@gmail.com>
2025-05-08 22:57:53 +0300

    changes

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -3,13 +3,21 @@ import { error } from 'console';
 import { AuthService } from '../../service/auth.service';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
-
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-profile',
   standalone: false,
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
+  animations: [
+    trigger('dialogFadeIn', [
+      transition(':enter', [ // when component enters the DOM
+        style({ opacity: 0, transform: 'scale(0.8)' }),
+        animate('300ms ease-out', style({ opacity: 1, transform: 'scale(1)' }))
+      ])
+    ])
+  ]
 })
 export class ProfileComponent implements OnInit{
   user: any;
