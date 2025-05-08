6b4fa9ddae17df3cde9948d238de23e63a43a1d9
Limbureuben <limbureuben4@gmail.com>
2025-05-08 23:25:29 +0300

    changes

--- src/app/user/forgot-password/forgot-password.component.ts
+++ src/app/user/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './forgot-password.component.scss'
 })
 export class ForgotPasswordComponent {
+
+  constructor(
+    private router: Router
+  ){}
+
+  goBack() {
+    this.router.navigate(['/']);
+  }
 
 }
