b57b18f8146e21d36425c5c25ac22b1379da3eab
Limbureuben <limbureuben4@gmail.com>
2025-05-09 22:47:38 +0300

    changes

--- src/app/booking/bookingdashboard/bookingdashboard.component.ts
+++ src/app/booking/bookingdashboard/bookingdashboard.component.ts
@@ -1,10 +1,23 @@
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-bookingdashboard',
   standalone: false,
   templateUrl: './bookingdashboard.component.html',
-  styleUrl: './bookingdashboard.component.scss'
+  styleUrl: './bookingdashboard.component.scss',
+  animations: [
+    trigger('cardStagger', [
+      transition(':enter', [
+        query('.dashboard-card', [
+          style({ opacity: 0, transform: 'translateY(20px)' }),
+          stagger(100, [
+            animate('500ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+          ])
+        ], { optional: true })
+      ])
+    ])
+  ]
 })
 export class BookingdashboardComponent {
 
