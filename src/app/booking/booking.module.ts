c95ae6c6564cfaeb2eb49cbb23d6c9521207a9bf
Limbureuben <limbureuben4@gmail.com>
2025-05-09 19:19:22 +0300

    changes

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -5,6 +5,27 @@ import { BookingRoutingModule } from './booking-routing.module';
 import { BookingdashboardComponent } from './bookingdashboard/bookingdashboard.component';
 import { BookingHeaderComponent } from './booking-header/booking-header.component';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+import { MatMenuModule } from '@angular/material/menu';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+
+
 
 @NgModule({
   declarations: [
@@ -13,7 +34,26 @@ import { BookingHeaderComponent } from './booking-header/booking-header.componen
   ],
   imports: [
     CommonModule,
-    BookingRoutingModule
+    BookingRoutingModule,
+    FormsModule,
+    MatSelectModule,
+    MatSidenavModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatListModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatPaginatorModule,
+    MatTableModule,
   ]
 })
 export class BookingModule { }
