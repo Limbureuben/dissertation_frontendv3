fa5e2e27b09a965e221a406f14702950603c50f9
Limbureuben <limbureuben4@gmail.com>
2025-05-09 23:30:25 +0300

    changes

--- src/app/booking/booking-map/booking-map.component.ts
+++ src/app/booking/booking-map/booking-map.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Map, MapStyle, config } from '@maptiler/sdk';
 
 @Component({
   selector: 'app-booking-map',
@@ -6,6 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './booking-map.component.html',
   styleUrl: './booking-map.component.scss'
 })
-export class BookingMapComponent {
+export class BookingMapComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  map: Map | undefined;
+
+  @ViewChild('map')
+  private mapContainer!: ElementRef<HTMLElement>;
+
+  ngOnInit(): void {
+    config.apiKey = '9rtSKNwbDOYAoeEEeW9B';
+  }
+
+  ngAfterViewInit() {
+    const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
+
+    this.map = new Map({
+      container: this.mapContainer.nativeElement,
+      style: MapStyle.STREETS,
+      center: [initialState.lng, initialState.lat],
+      zoom: initialState.zoom
+    });
+  }
+
+  ngOnDestroy() {
+    this.map?.remove();
+  }
 
 }
