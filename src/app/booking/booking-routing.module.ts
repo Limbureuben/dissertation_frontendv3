243d376fdc9e72029c73eb1a45d846739492755a
Limbureuben <limbureuben4@gmail.com>
2025-05-09 23:00:13 +0300

    changes

--- src/app/booking/booking-routing.module.ts
+++ src/app/booking/booking-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookingdashboardComponent } from './bookingdashboard/bookingdashboard.component';
+import { BookingMapComponent } from './booking-map/booking-map.component';
 
 const routes: Routes = [
-  { path: 'booking-dashboard', component: BookingdashboardComponent }
+  { path: 'booking-dashboard', component: BookingdashboardComponent },
+  { path: 'booking-map', component: BookingMapComponent }
 ];
 
 @NgModule({
