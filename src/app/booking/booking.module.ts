6b2b2a8768d3616fc30da973fd438ecc6bf0d5c5
Limbureuben <limbureuben4@gmail.com>
2025-05-09 18:58:25 +0300

    changes

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { BookingRoutingModule } from './booking-routing.module';
 import { BookingdashboardComponent } from './bookingdashboard/bookingdashboard.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    BookingdashboardComponent
+    BookingdashboardComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
