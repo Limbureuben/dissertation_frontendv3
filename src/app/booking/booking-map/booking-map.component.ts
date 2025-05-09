752090d858543a21f1b886b5ac3171275ebd3b7c
Limbureuben <limbureuben4@gmail.com>
2025-05-09 23:49:59 +0300

    changes

--- src/app/booking/booking-map/booking-map.component.ts
+++ src/app/booking/booking-map/booking-map.component.ts
@@ -1,5 +1,6 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild, PLATFORM_ID, Inject } from '@angular/core';
 import { Map, MapStyle, config } from '@maptiler/sdk';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-booking-map',
@@ -10,27 +11,37 @@ import { Map, MapStyle, config } from '@maptiler/sdk';
 export class BookingMapComponent implements OnInit, AfterViewInit, OnDestroy {
 
   map: Map | undefined;
+  isBrowser: boolean;
 
   @ViewChild('map')
   private mapContainer!: ElementRef<HTMLElement>;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
   ngOnInit(): void {
-    config.apiKey = '9rtSKNwbDOYAoeEEeW9B';
+    if (this.isBrowser) {
+      config.apiKey = '9rtSKNwbDOYAoeEEeW9B';
+    }
   }
 
   ngAfterViewInit() {
-    const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
+    if (this.isBrowser) {
+      const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
 
-    this.map = new Map({
-      container: this.mapContainer.nativeElement,
-      style: MapStyle.STREETS,
-      center: [initialState.lng, initialState.lat],
-      zoom: initialState.zoom
-    });
+      this.map = new Map({
+        container: this.mapContainer.nativeElement,
+        style: MapStyle.STREETS,
+        center: [initialState.lng, initialState.lat],
+        zoom: initialState.zoom
+      });
+    }
   }
 
   ngOnDestroy() {
-    this.map?.remove();
+    if (this.isBrowser) {
+      this.map?.remove();
+    }
   }
-
 }
