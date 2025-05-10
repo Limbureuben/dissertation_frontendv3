5ef9c4d4de7a33cc05b15ce57cd9f2baf3464967
Limbureuben <limbureuben4@gmail.com>
2025-05-10 19:17:33 +0300

    changes

--- src/app/guards/no-token.guard.ts
+++ src/app/guards/no-token.guard.ts
@@ -1,5 +1,19 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
 
-export const noTokenGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class NoTokenGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(): boolean {
+    const token = localStorage.getItem('token');
+    if (token) {
+      // If token exists, redirect to homepage
+      this.router.navigate(['/homepage']);
+      return false;
+    }
+    return true; // Allow access only if token does NOT exist
+  }
+}
