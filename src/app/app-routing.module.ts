77271716f3e23671db014599430d19035c64b855
Limbureuben <limbureuben4@gmail.com>
2025-05-10 21:51:58 +0300

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { MapAdminComponent } from './admin/map-admin/map-admin.component';
 import { CustomerSidebarComponent } from './admin/customer-sidebar/customer-sidebar.component';
 import { WardCustomersidebarComponent } from './wardexecutive/ward-customersidebar/ward-customersidebar.component';
 import { BookingHeaderComponent } from './booking/booking-header/booking-header.component';
+import { Router, NavigationEnd } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/user-home', pathMatch: 'full'},
@@ -106,4 +107,15 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+  constructor(
+    router: Router
+  ) {
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        localStorage.setItem('lastRoute', event.urlAfterRedirects);
+      }
+    });
+  }
+}
