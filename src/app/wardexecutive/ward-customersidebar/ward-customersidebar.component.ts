37864687ddc3ea017b13136959f588694616b187
Limbureuben <limbureuben4@gmail.com>
2025-05-11 13:31:57 +0300

    changes

--- src/app/wardexecutive/ward-customersidebar/ward-customersidebar.component.ts
+++ src/app/wardexecutive/ward-customersidebar/ward-customersidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 export type MenuItem = {
   icon: string;
@@ -22,12 +24,21 @@ export class WardCustomersidebarComponent {
   ])
 
   constructor(
-
+    private toastr: ToastrService,
+    private router: Router
   ){}
 
   onLogout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('role');
 
+    this.toastr.success('Logout success', '', {
+      timeOut: 1500,
+      progressBar: true,
+      positionClass: 'toast-top-right'
+    });
+    setTimeout(() => {
+      this.router.navigate(['/user-home']);
+    }, 1500);
   }
-
-
 }
