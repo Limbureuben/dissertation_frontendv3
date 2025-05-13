a9311d94e43f171455504011e1f22d3893a34c2d
Limbureuben <limbureuben4@gmail.com>
2025-05-13 23:55:54 +0300

    changes

--- src/app/admin/customer-sidebar/customer-sidebar.component.ts
+++ src/app/admin/customer-sidebar/customer-sidebar.component.ts
@@ -16,14 +16,13 @@ export type MenuItem = {
 export class CustomerSidebarComponent {
   MenuItems = signal<MenuItem[]>([
     { icon: 'dashboard', label: 'Dashboard', route: 'dash' },
-    { icon: 'map', label: 'Map', route: 'map-common' },
-    { icon: 'map', label: 'Booking Map', route: 'map-common' },
-    { icon: 'location_on', label: 'OpenSpace', route: 'openspace' },
-    { icon: 'report', label: 'Report', route: 'reports' },
-    { icon: 'report', label: 'Report-USSD', route: 'report-ussd' },
-    { icon: 'report', label: 'Executive', route: 'manage-wardexecutive' },
+    { icon: 'public', label: 'Map', route: 'map-common' },
+    { icon: 'place', label: 'OpenSpace', route: 'openspace' },
+    { icon: 'feedback', label: 'Report', route: 'reports' },
+    { icon: 'sms', label: 'Report-USSD', route: 'report-ussd' },
+    { icon: 'supervisor_account', label: 'Executive', route: 'manage-wardexecutive' },
     { icon: 'history', label: 'History', route: 'history' },
-    { icon: 'map', label: 'TestMap', route: 'test-map' },
+    { icon: 'explore', label: 'TestMap', route: 'test-map' },
     { icon: 'logout', label: 'Logout' }
   ])
 
