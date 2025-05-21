37fd279ccb5b1fa66fdd26ac5709771dae58df07
Limbureuben <limbureuben4@gmail.com>
2025-05-21 11:31:32 +0300

    changes

--- src/app/wardexecutive/ward-booking/ward-booking.component.ts
+++ src/app/wardexecutive/ward-booking/ward-booking.component.ts
@@ -6,12 +6,33 @@ import { MatSort } from '@angular/material/sort';
 import { ToastrService } from 'ngx-toastr';
 import { MatDialog } from '@angular/material/dialog';
 import { AvailableBookingComponent } from '../available-booking/available-booking.component';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-ward-booking',
   standalone: false,
   templateUrl: './ward-booking.component.html',
-  styleUrl: './ward-booking.component.scss'
+  styleUrl: './ward-booking.component.scss',
+  animations: [
+    // Slide table from right when opening
+    trigger('tableEnterAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateX(50px)' }),
+        animate('500ms ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+      ])
+    ]),
+
+    // Animate row additions & deletions
+    trigger('rowAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-10px)' }),
+        animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ opacity: 0, transform: 'translateY(10px)' }))
+      ])
+    ])
+  ]
 })
 export class WardBookingComponent implements OnInit{
   dataSource = new MatTableDataSource<any>([]);
