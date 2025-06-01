08be3069bd73eb2fcf7e28ed4de68287cf40e1f4
Limbureuben <limbureuben4@gmail.com>
2025-06-01 12:40:12 +0300

    changes

--- src/app/booking/booking-header/booking-header.component.ts
+++ src/app/booking/booking-header/booking-header.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { BookingHistoryComponent } from '../booking-history/booking-history.component';
 
 @Component({
   selector: 'app-booking-header',
@@ -12,7 +14,8 @@ export class BookingHeaderComponent {
 
   constructor(
     private router: Router,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private dialog: MatDialog
   ) {}
 
 
@@ -51,6 +54,14 @@ export class BookingHeaderComponent {
 
   NavigateToCalender() {
     this.router.navigate(['/calender']);
+  }
+
+  openReportHistoryDialog() {
+    this.dialog.open(BookingHistoryComponent, {
+      width: '1200px',
+      height: '500px',
+      disableClose: false
+    });
   }
 
 }
