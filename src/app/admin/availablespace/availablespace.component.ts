3a9f9ff538ad83566c4e97a3f171bb17e0c78237
Limbureuben <limbureuben4@gmail.com>
2025-06-07 13:37:19 +0300

    changes

--- src/app/admin/availablespace/availablespace.component.ts
+++ src/app/admin/availablespace/availablespace.component.ts
@@ -69,7 +69,7 @@ export class AvailablespaceComponent implements OnInit{
       text: "You won't be able to revert this!",
       icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: "#3085d6",
+      confirmButtonColor: "rgb(100, 100, 177)",
       cancelButtonColor: "#d33",
       confirmButtonText: "Yes, delete it!"
     }).then((result) => {
